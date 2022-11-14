d0115e06e0562a339d4303d6e072bd5963c0ab33
Atul Kumar Pandey <atul.pandey@cloudeq.com>
2022-11-14 13:57:04 +0530

    demo codestream

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+
+app.get("/", (req, res) => {
+
+  res.json([
+
+    {
+
+      id: "001",
+      name: "atul",
+      role: "DevOps",
+
+    },
+
+  ])
+
+})
+
+app.listen(8080, () => {
+
+  console.log("listning from port 8080")
+
+})
